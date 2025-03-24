b445c1fd627a3a21c4ec6b34a5aa05d1387ea17f
VLmb <vlamb0809@gmail.com>
2025-03-24 09:52:08 +0300

    Fixed some mistakes

--- neuro_part/app.py
+++ neuro_part/app.py
@@ -3,7 +3,7 @@ from api import api_bp
 
 app = Flask(__name__)
 
-app.register_blueprint(api_bp, url_prefix='/api')
+app.register_blueprint(api_bp, url_prefix='/api.neuro_part')
 
 if __name__ == '__main__':
     app.run(debug=True)
