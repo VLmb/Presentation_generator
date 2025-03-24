684911c89647672309e58c76f1877b7bc0767cea
VLmb <vlamb0809@gmail.com>
2025-03-24 09:47:48 +0300

    Add neuro_part API

---
+++ neuro_part/api.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, request, jsonify
+from json_handler import parse_json_with_def_params, parse_json_with_text
+
+api_bp = Blueprint('api.neuro_part', __name__)
+
+@api_bp.route('/gen_by_def_params', methods = ['POST'])
+def gen_by_def_params():
+    data = request.get_json()
+    parse_json_with_def_params(data)
+    return jsonify({
+        'massage': 'Богдан бусинка, все воркает)). Дефолтные параметры передались.'
+    })
+
+@api_bp.route('/gen_by_text', methods = ['POST'])
+def gen_by_text():
+    data = request.get_json()
+    parse_json_with_text(data)
+    return jsonify({
+        'massage': 'Богдан бусинка, все воркает)). Текст передался'
+    })
--- neuro_part/json_handler.py
+++ neuro_part/json_handler.py
@@ -1,3 +1,14 @@
-
-def parse_json(json_file):
+#название, количество слайдов
+def parse_json_with_def_params(json_file):
     pass
+#название, текст
+def parse_json_with_text(json_file):
+    pass
+
+'''
+Сгенерированный json можешь отправить в бэк можешь передать с помощью след кода:
+headers = {"Content-Type": "application/json", "User-Agent": "MyApp"}
+api_backend = "api.backend\create_presentation"
+response = requests.post(api_backend, json=data, headers=headers)
+data - json файл
+'''
